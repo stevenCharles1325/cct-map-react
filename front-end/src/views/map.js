c726e8779713dcbd97b99e27316f057cbc9232a4
Steven Charles <stevencharles1325@gmail.com>
2021-08-06 20:13:00 +0800

    Fixed wireframe display on rendering object

--- front-end/src/views/map.js
+++ front-end/src/views/map.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect, useRef, Suspense } from 'react';
 
 import { 
-            OrbitControls,
-            Stars,
-            Html, 
-            useProgress 
-
-        } from '@react-three/drei';
+    OrbitControls,
+    Stars,
+    Html, 
+    useProgress,
+    SpotLight
+} from '@react-three/drei';
 
 import { Canvas, useFrame, useLoader } from '@react-three/fiber';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
@@ -19,41 +19,48 @@ import '../styles/map.css';
 // Initialize the map
 // Create land
 // Create import handler
-
+const LAND_SIZE = [5000, 5000];
 
 function MapView( props ){
-    const [upload, setUpload] = useState( null );
     const [objectList, setObjectList] = useState( [] );
-
-    const requestSetUpload = ( object_data ) => {
-        console.log(upload)
-        setUpload( object_data );
-    }
+    const landRef = useRef();
 
 
-    useEffect(() => {
 
-        if( upload ){
-            console.log( upload );
-            setObjectList(
-                    <Scene filePath={upload.filePath} />
-                );
-            setUpload( null );    
+    // ------------------------------------------
+    // 
+    //              Upload request
+    // 
+    // ------------------------------------------
+        const [upload, setUpload] = useState( null );
+
+        const requestSetUpload = ( object_data ) => {
+            setUpload( object_data );
         }
-        
-    }, [upload]);
+
+
+        useEffect(() => {
+
+            if( upload ){
+                setObjectList( <Imported  file={upload} /> );
+                setUpload( null );    
+            }
+            
+        }, [upload]);
+    // -------------------------------------------
+
+
 
     return (
         <div className="map">
             <MapMenu reqSetUpload={requestSetUpload} />
             <Canvas
                 camera={{position: [0, 2500, -2500], far: 10000}}
+
             >
                 <Suspense fallback={<Loader />}>
-
-                    { console.log(Canvas) }
                     <MapEssentials />
-                    <Land size={[5000, 5000]}/>
+                    <Land landRef={landRef} size={[5000, 5000]}/>
 
                     <Suspense fallback={<Loader />}>
                         { objectList }
@@ -69,43 +76,15 @@ function MapView( props ){
 
 
 
-
 // ------------------------------------------------
 // 
-// 
-//                 MAP IMPORT HANDLER
-// 
+//                Simple loading
 // 
 // ------------------------------------------------
-
-function Loader() {
-    const { progress } = useProgress();
-    console.log( progress );
-    return <Html center> {progress} % loaded </Html>
-}
-
-
-function Scene( props ) {
-    const obj = useLoader(OBJLoader, props.filePath );
-    const obj_ref = useRef();
-
-    useEffect(() => {
-        if( obj_ref.current ){
-            for( let child of obj_ref.current.children ){
-                for( let material of child.material ){
-                    material = new THREE.MeshStandardMaterial();
-                }
-            }
-        }
-    });
-
-    return (
-        <primitive ref={obj_ref} object={obj} scale={50} >
-            { obj_ref.current ? console.log( obj_ref ) : null }
-            <meshStandardMaterial color="black" wireFrame={false}/>
-        </primitive>
-    );
-}
+    function Loader() {
+        return <Html center> LOADING... </Html>
+    }
+// ------------------------------------------------
 
 
 
@@ -115,34 +94,110 @@ function Scene( props ) {
 
 // ------------------------------------------------
 // 
+//                 MAP IMPORT HANDLER
 // 
-//          MAP LAND AND OTHER ESSENTIALS
+// ------------------------------------------------
+
+    function Imported( props ) {
+        const imported_object = useLoader(OBJLoader, props.file.filePath );
+
+        const reqSetSelected = (e) => {
+            console.log( e );
+            
+        }
+
+        const { objRef, isSelected, setIsSelected, reqClickObj } = useClickHandler(reqSetSelected);
+
+        useFrame(() => {
+            
+        });
+
+        return (
+            <group ref={objRef} onClick={reqClickObj} receiveShadow castShadow>
+                {imported_object.children.map( child => (
+                    <mesh key={`${props.file.fileName}_${imported_object.children.indexOf( child )}`} scale={50} geometry={child.geometry}>
+                        <meshStandardMaterial color="white" metalness={0.3} roughness={0.5}/>
+                    </mesh>
+                    ) 
+                )}
+            </group>
+        );
+    }
+// ------------------------------------------------
+
+
+
+
+
+
+
+function Copy( props ){
+
+}
+
+// ------------------------------------------------
+
+
+
+// ================================================
+
+
+
+// ------------------------------------------------
 // 
+//              ESSENTIALS AND LOGICS
 // 
 // ------------------------------------------------
 
 function MapEssentials( props ){
+    const spotLightRef = useRef();
+
+    useEffect(() => {
+        if(spotLightRef.current){
+            spotLightRef.current.lookAt(0, 0, 0);
+        }
+    })
+
     return (
         <>
             <Stars radius={2500} count={10000} fade />
             <OrbitControls/>
-            <ambientLight intensity={0.4}/>
+            <ambientLight intensity={0.3}/>
+            <spotLight ref={spotLightRef} castShadow position={[1000, 5000, 0]}/>
         </>
     );
 }
 
 
 function Land( props ){
-    const mesh = useRef();
 
     return (
-        <group>
-            <mesh ref={mesh} visible rotation={[-Math.PI / 2, 0, 0]} >
-                <planeBufferGeometry receiveShadow args={props.size || 1}/>
-                <meshStandardMaterial color={props.color || "white"}/>
+        <group receiveShadow>
+            <mesh ref={props.landRef} visible rotation={[-Math.PI / 2, 0, 0]} >
+                <planeBufferGeometry args={props.size || 1}/>
+                <meshStandardMaterial color={props.color || "white"} roughness={0.9} metalness={0.5}/>
             </mesh>
         </group>
     );
 }
+
+
+function useClickHandler( callback ){
+    const objectRef = useRef();
+    const [isSelected, setIsSelected] = useState( false );
+    
+    const reqClickObj = (e) => {
+        console.log('Clicked');
+        callback(e);
+    }
+
+    return { 
+        objectRef, 
+        isSelected, 
+        setIsSelected, 
+        reqClickObj
+    };
+}
+
 
 export default MapView;
